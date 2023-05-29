eab688099d8cf192f6d9f5092a17c77e8c42aa8e
Rendxnn <88605945+Rendxnn@users.noreply.github.com>
2023-05-29 14:34:41 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from machine import Pin, ADC, PWM
 import time
 
 
-UPLINK_KEY = "<UPLINK_KEY>"
+UPLINK_KEY = "server_UTNEKVEQD253TFF3B7ODWCNZ-2UHY4HPVWF6UUX2R"
 
 
 led = Pin("LED", Pin.OUT, value=1)
@@ -12,33 +12,41 @@ pot = ADC(Pin(28))
 motor = PWM(Pin(27))
 boton = Pin(16, Pin.IN, Pin.PULL_DOWN)
 led1 = Pin(17, Pin.OUT, value=1)
+pito1 = Pin(19, Pin.OUT, value=0)
 motor.freq(1000)
 
 
-@anvil.pico.callable(is_async=True)
+@anvil.pico.callable_async
 async def leer_potenciometro():
-    return pot.read_u16()
+    return int(pot.read_u16())
 
 
-@anvil.pico.callable(is_async=True)
+@anvil.pico.callable_async
 async def activar_motor():
-    duty = leer_potenciometro()
+    duty = int(pot.read_u16())
     motor.duty_u16(duty)
     
 
-@anvil.pico.callable(is_async=True)
+@anvil.pico.callable_async
 async def apagar_motor():
     motor.duty_u16(0)
     
 
-@anvil.pico.callable(is_async=True)
+@anvil.pico.callable_async
 async def leer_boton():
     return boton.value()
 
 
-@anvil.pico.callable(is_async=True)
+@anvil.pico.callable_async
 async def led():
     led1.toggle()
+    
+    
+@anvil.pico.callable_async
+async def pito():
+    pito1.on()
+    time.sleep(1000)
+    pito1.off()
 
 
 anvil.pico.connect(UPLINK_KEY)
